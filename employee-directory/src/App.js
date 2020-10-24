8b14b5c27f73077f14d6ca09df14c95b5c090ea6
JordanB <65307981+ubern00bie@users.noreply.github.com>
2020-10-23 19:55:17 -0500

    added basic component structure

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './components/Table';
+import Container from "./components/Container";
+import Table from "./components/Table";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
+const headerText = 'Employee Directory';
+//be able to sort by at least one category
+//filter users by at least one property
 function App() {
   return (
+  
     <div className="App">
+       
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Header headerText = {headerText}/>
+      <Navbar/>
+        <Container style={{ minHeight: "80%"  }}>
+        <Table/>
+        </Container>
       </header>
     </div>
   );
---
+++ employee-directory/src/components/Navbar/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import './Styles.css';
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <a href="/">Welcome</a>
+        </nav>
+    );
+}
+
+export default Navbar;
