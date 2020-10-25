4bd1e90a4a0a2bdafbf067e27fcb97dd545a2c27
JordanB <65307981+ubern00bie@users.noreply.github.com>
2020-10-25 00:48:14 -0500

    Updates

---
+++ precache-manifest.bf9eb521e0f1b0ba697326d8306ceb9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b48c3f24d7a2676e2d25974ce50608a9",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "9e0b820c0f1fb80c9616",
+    "url": "/Employee-Directory/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "1bc8345f56b8aa7d3422",
+    "url": "/Employee-Directory/static/css/main.486fb40e.chunk.css"
+  },
+  {
+    "revision": "9e0b820c0f1fb80c9616",
+    "url": "/Employee-Directory/static/js/2.3b91f975.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/Employee-Directory/static/js/2.3b91f975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bc8345f56b8aa7d3422",
+    "url": "/Employee-Directory/static/js/main.34ff491a.chunk.js"
+  },
+  {
+    "revision": "21ab4dbde2f8e2bfdf72",
+    "url": "/Employee-Directory/static/js/runtime-main.965eecad.js"
+  }
+]);
